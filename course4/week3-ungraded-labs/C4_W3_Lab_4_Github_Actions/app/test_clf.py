a80380b934bd232b4ba6fec70823669a1f7d5088
tosy <tosy@tosys-MBP.attlocal.net>
2024-04-13 11:24:30 -0700

    testing CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,5 +1,20 @@
 import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from main import clf
+
+
+def test_pipeline_and_scaler():
+
+    # Check if clf is an instance of sklearn.pipeline.Pipeline
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+    
+    if isPipeline:
+        # Check if first step of pipeline is an instance of
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
 
 
 def test_accuracy():
