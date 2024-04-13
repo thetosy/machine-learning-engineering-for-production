82a37ac300194078c932200c1280d13a93b7a7c6
tosy <tosy@tosys-MBP.attlocal.net>
2024-04-13 11:16:45 -0700

    testing CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -9,6 +9,7 @@ from pydantic import BaseModel, conlist
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
+# this is used in test_vlf
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
